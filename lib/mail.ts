6f65a8198313cfcb67a627054635338b29cb99ce
arhitectsrb <sladjanscg@gmail.com>
2025-08-17 21:31:43 +0200

    Update mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,14 +1,18 @@
 import { Resend } from 'resend'
 
-// koristimo nerezerivisana imena
 const resendKey = process.env.MY_RESEND_KEY
-const fromAddr  = process.env.MY_MAIL_FROM || 'MJ Studio <miksi1996@gmail.com>'
+const fromAddr  = process.env.MY_MAIL_FROM || 'MJ Studio <onboarding@resend.dev>'
 
 export async function sendMail(to: string, subject: string, html: string) {
   if (!resendKey) {
-    console.warn('MY_RESEND_KEY not set; skipping email to', to, subject)
+    console.error('[MAIL] MY_RESEND_KEY not set')
     return
   }
   const resend = new Resend(resendKey)
-  await resend.emails.send({ from: fromAddr, to, subject, html })
+  try {
+    const res = await resend.emails.send({ from: fromAddr, to, subject, html })
+    console.log('[MAIL] Sent:', res?.id || res)
+  } catch (err: any) {
+    console.error('[MAIL] Error:', err?.message || err)
+  }
 }
